888b5c7e356307ae68efa38b117c0457c915eca6
ale-wp <ale-wp345@yandex.ru>
2024-10-08 18:00:49 +0300

    Finished converted color in HEX to RGB

--- h4-1/src/components/ConverterHEXToRGB.tsx
+++ h4-1/src/components/ConverterHEXToRGB.tsx
@@ -1,6 +1,10 @@
 import {useState} from "react";
+import '../assets/converterHexToRgb.css';
 
 export default function ConverterHEXToRGB() {
+
+    const body = document.getElementsByTagName('body')[0];
+    const default_style = 'background-color:rgb(255,0,0);';
 
     const[form,setForm] = useState({
         hexField: '',
@@ -8,21 +12,37 @@ export default function ConverterHEXToRGB() {
 
     const handleNameChange = ({target}) => {
         const {name, value} = target;
-        console.log(name);
-        console.log(value);
-        setForm(prevForm => ({...prevForm, [name]: value}));
+        // console.log(name);
+        // console.log(value);
+        if(value.length > 7) {
+            setForm(prevForm => ({...prevForm, [name]: value.substring(0,7)}));
+        } else {
+            setForm(prevForm => ({...prevForm, [name]: value}));
+        }
     }
 
     const convertValue = (val) => {
-        console.log(val);
-        if(val.includes('#') === false && val !== '') return (<>#ошибка</>);
+        if(val.length === 7) {
+            if(val.includes('#') === false) {
+                body.setAttribute('style', default_style)
+                return (<>#ошибка</>);
+            }
 
+            const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(val);
 
-        return (
-            <>
-                {val}
-            </>
-        );
+            body.setAttribute('style', result ? 'background-color:rgb(' + parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) + ')' : default_style);
+
+            return (
+                <>
+                    { result ? 'rgb(' + parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) + ')' : '#ошибка'}
+                </>
+            );
+        } else if(val.length > 7) {
+            return (<>{val.substring(0,7)}</>);
+        } else {
+            body.removeAttribute('style');
+            return (<></>);
+        }
     }
 
     return (
