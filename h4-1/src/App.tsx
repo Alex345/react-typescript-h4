13acb89c8ce44668df94ea7e4159ad5613434411
ale-wp <ale-wp345@yandex.ru>
2024-10-15 22:52:14 +0300

    Update code with app state

--- h4-1/src/App.tsx
+++ h4-1/src/App.tsx
@@ -2,8 +2,34 @@ import './App.css'
 import ConverterHEXToRGB from "./components/ConverterHEXToRGB";
 
 function App() {
+
+  const default_style = 'background-color:rgb(255,0,0);';
+
+  const convertValue = (val) => {
+    if(val.includes('#') === false && val.length !== 0) {
+        root.setAttribute('style', default_style);
+        return (<>#ошибка</>);
+    } else if (val.length === 7) {
+
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(val);
+
+      root.setAttribute('style', result ? 'background-color:rgb(' + parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) + ');' : default_style);
+
+      return (
+          <>
+            {result ? 'rgb(' + parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) + ')' : '#ошибка'}
+          </>
+      );
+    } else if (val.length > 7) {
+      return (<>{val.substring(0, 7)}</>);
+    } else {
+      root.removeAttribute('style');
+      return (<></>);
+    }
+  }
+
   return (
-    <ConverterHEXToRGB/>
+    <ConverterHEXToRGB fn={convertValue}/>
   )
 }
 
--- h4-1/src/components/ConverterHEXToRGB.tsx
+++ h4-1/src/components/ConverterHEXToRGB.tsx
@@ -1,19 +1,15 @@
 import {useState} from "react";
 import '../assets/converterHexToRgb.css';
 
-export default function ConverterHEXToRGB() {
-
-  const body = document.getElementsByTagName('body')[0];
-  const default_style = 'background-color:rgb(255,0,0);';
+export default function ConverterHEXToRGB({fn}) {
 
   const [form, setForm] = useState({
     hexField: '',
   });
 
-  const handleNameChange = ({target}) => {
+  const handleNameChange = ({target}: ChangeEvent<HTMLInputElement>) => {
     const {name, value} = target;
-    // console.log(name);
-    // console.log(value);
+
     if (value.length > 7) {
       setForm(prevForm => ({...prevForm, [name]: value.substring(0, 7)}));
     } else {
@@ -21,41 +17,19 @@ export default function ConverterHEXToRGB() {
     }
   }
 
-  const convertValue = (val) => {
-    if (val.length === 7) {
-      if (val.includes('#') === false) {
-        body.setAttribute('style', default_style)
-        return (<>#ошибка</>);
-      }
-
-      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(val);
-
-      body.setAttribute('style', result ? 'background-color:rgb(' + parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) + ')' : default_style);
-
-      return (
-        <>
-          {result ? 'rgb(' + parseInt(result[1], 16) + ',' + parseInt(result[2], 16) + ',' + parseInt(result[3], 16) + ')' : '#ошибка'}
-        </>
-      );
-    } else if (val.length > 7) {
-      return (<>{val.substring(0, 7)}</>);
-    } else {
-      body.removeAttribute('style');
-      return (<></>);
-    }
-  }
-
   return (
     <form>
-      <label htmlFor="hexField">
-        Введите цвет в формате HEX для преобразования в RGB
-      </label>
-      <input
-        type="text"
-        id="hexField" name="hexField"
-        value={form.hexField}
-        onChange={handleNameChange}/>
-      <span>{convertValue(form.hexField)}</span>
+      <div className="form__wrapper">
+        <label htmlFor="hexField">
+          Введите цвет в формате HEX для преобразования в RGB
+        </label>
+        <input
+          type="text"
+          id="hexField" name="hexField"
+          value={form.hexField}
+          onChange={handleNameChange}/>
+        <span>{fn(form.hexField)}</span>
+      </div>
     </form>
   )
 }
